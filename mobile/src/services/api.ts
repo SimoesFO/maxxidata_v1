40bd328ea11d65de4e026d73a3def5162bdf3a91
Felipe Simoes <felipeoliveirasimoes@hotmail.com>
2021-07-18 22:50:20 -0300

    Aplicação Mobile - React Native

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const PORT = 3333;
+const HOST = '192.168.15.5';
+// const HOST = 'localhost';
+
 const api = axios.create({
-  baseURL: "http://192.168.15.5:3333/api/v1",
+  baseURL: `http://${HOST}:${PORT}/api/v1`,
 });
 
 export default api;
